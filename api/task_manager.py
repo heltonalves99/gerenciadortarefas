b1170590de83ccab59f3c65b8b1d3200ec5af03d
heltonalves99 <helton99@hotmail.com>
2019-08-06 11:36:47 -0300

    Create task.

--- api/task_manager.py
+++ api/task_manager.py
@@ -1,10 +1,28 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, abort
 
-# bp = Blueprint('task_manager', __name__, url_prefix='/todo')
 bp = Blueprint("task_manager", __name__)
 
 TASKS = []
 
+
 @bp.route("/")
 def listar():
     return jsonify(TASKS)
+
+
+@bp.route("/", methods=["POST"])
+def create():
+    
+    if not request.data \
+    or not request.json.get("title") \
+    or not request.json.get("description"):
+        abort(400)
+
+    task = {
+        "id": len(TASKS) + 1,
+        "title": request.json.get("title"),
+        "description": request.json.get("description"),
+        "state": False,
+    }
+    TASKS.append(task)
+    return jsonify(task), 201
--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -1,3 +1,5 @@
+import json
+
 from api.task_manager import TASKS
 
 
@@ -32,8 +34,9 @@ def test_list_tasks_return_not_empty_list(client):
     """
         should return a list with todo task.
     """
-    task = {"id": 1, "titulo": "tarefa 1",
-            "descricao": "tarefa de numero 1", "estado": False}
+
+    task = {"id": 1, "title": "title 1",
+            "description": "description 1", "state": False}
     
     TASKS.append(task)
 
@@ -41,3 +44,95 @@ def test_list_tasks_return_not_empty_list(client):
     assert resp.json == [task]
 
     TASKS.clear()
+
+
+def test_create_task_accept_post(client):
+    """
+        should accept post request in create task.
+    """
+
+    resp = client.post("/todo/")
+
+    assert resp.status_code != 405
+
+
+def test_create_task_return_created_task(client):
+    """
+        should create a task and return it.
+    """
+
+    TASKS.clear()
+    mock_data = {
+        "title": "my title",
+        "description": "my description",
+    }
+
+    resp = client.post("/todo/", json=mock_data)
+
+    data = json.loads(resp.data.decode("utf-8"))
+    assert data["id"] == 1
+    assert data["title"] == mock_data["title"]
+    assert data["description"] == mock_data["description"]
+    assert data["state"] is False
+
+
+def test_create_task_return_status_code_201(client):
+    """
+        should return status code 201 when to create a task.
+    """
+
+    TASKS.clear()
+    mock_data = {
+        "title": "my title",
+        "description": "my description",
+    }
+
+    resp = client.post("/todo/", json=mock_data)
+
+    assert resp.status_code == 201
+
+
+def test_create_task_insert_database(client):
+    """
+        should create a task and insert it in database.
+    """
+
+    TASKS.clear()
+    mock_data = {
+        "title": "my title",
+        "description": "my description",
+    }
+
+    client.post("/todo/", json=mock_data)
+
+    assert len(TASKS) > 0
+
+
+def test_create_task_without_title(client):
+    """
+        should return status code 400 if data does not have title.
+    """
+
+    TASKS.clear()
+    mock_data = {
+        "description": "my description",
+    }
+
+    resp = client.post("/todo/", json=mock_data)
+
+    assert resp.status_code == 400
+
+
+def test_create_task_without_description(client):
+    """
+        should return status code 400 if data does not have description.
+    """
+
+    TASKS.clear()
+    mock_data = {
+        "title": "my title",
+    }
+
+    resp = client.post("/todo/", json=mock_data)
+
+    assert resp.status_code == 400
